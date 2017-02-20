34b906bef54640359b22558a7a5058c419ce1a6d
Benjamin Saphier <b.saphier@gmail.com>
2017-02-19 21:22:13 -0500

    default commit message

--- server/app/index.js
+++ server/app/index.js
@@ -1,14 +1,29 @@
 const path = require('path');
+const morgan = require('morgan');
 const express = require('express');
-
-const app = express();
-module.exports = app;
+const bodyParser = require('body-parser');
 
 const staticMiddleware = require('./static.middleware');
 
-app.set('port', (process.env.PORT || 1337));
+const app = express();
+const logger = morgan('dev');
+module.exports = app;
+
+app.use(logger);
 
 app.use(staticMiddleware);
+
+app.set('port', (process.env.PORT || 1337));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// error handling middleware
+app.use((error, req, res, next) => {
+  error.status = error.status || 500;
+  error.message = error.message || 'Internal Error';
+  res.status(error.status).send(error.message);
+});
 
 app.get('*', (req, res, next) => {
   res.sendFile(path.join(__dirname, '..', '..', 'index.html'));
