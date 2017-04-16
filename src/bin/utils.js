7e9c00767579ffccc3127777e957f64aa1d53732
Benjamin Saphier <b.saphier@gmail.com>
2017-04-16 18:22:41 -0400

    projectpanel parallax effect finished, for now

--- src/bin/utils.js
+++ src/bin/utils.js
@@ -1,1 +1,12 @@
 export const int = float => parseInt(float, 10);
+
+const _normHelpA = (max, min, newMax, newMin) => ((newMax - newMin) / (max - min));
+const _normHelpB = (max, newMax, paramA) => (newMax - paramA * max);
+
+// this returns a func that will normalize an input to the range provided here
+export const normal = (max, min, newMax, newMin) => {
+  let paramA = _normHelpA(max, min, newMax, newMin);
+  let paramB = _normHelpB(max, newMax, paramA);
+
+  return ( value ) => (paramA * value + paramB);
+};
--- src/components/ProjectPanel.js
+++ src/components/ProjectPanel.js
@@ -2,32 +2,26 @@ import React, { Component } from 'react';
 
 import Modernizr from '../../.modernizrrc';
 import { buttons, Cell, Title, FillSection } from './displayComponents';
-import { int } from '../bin/utils';
+import { normal as _normal } from '../bin/utils';
 const { Button } = buttons;
 
 const transform = Modernizr.prefixed('transform');
 
 const styles = {
   parallaxWrap: {
-    position: 'relative',
+    // position: 'relative',
     overflow: 'hidden',
     width: '100%',
     height: '100%',
-
-    WebkitPerspective: 'inherit',
-    MozPerspective: 'inherit',
-    OPerspective: 'inherit',
-    perspective: 'inherit',
+    backgroundColor: 'rgb(81, 81, 81)'
   },
   parallaxDiv: {
     //:TODO parallax styles . . .
     position: 'absolute',
+    width: '100%',
+    height: '100%',
+    left: '3vmin',
     backgroundColor: 'transparent',
-
-    WebkitTransformStyle: 'preserve-3d',
-    MozTransformStyle: 'preserve-3d',
-    OTransformStyle: 'preserve-3d',
-    transformStyle: 'preserve-3d'
   },
 
   background: {
@@ -86,87 +80,148 @@ const styles = {
 
 class ProjectPanel extends Component {
 
-  constructor(props) {
+  constructor( props ) {
     super(props);
     this.state = {
       mouseX: 0,
-      mouseY: 0
+      mouseY: 0,
+      panelWidth: 0,
+      panelHeight: 0,
+      parallax: Array.isArray(props.project.backgroundImg)
     };
 
     this.getElement = this.getElement.bind(this);
+    this.normMouseX = this.normMouseX.bind(this);
+    this.normMouseY = this.normMouseY.bind(this);
+    this.handleResize = this.handleResize.bind(this);
     this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.createBackground = this.createBackground.bind(this);
+  }
+
+
+  componentDidMount() {
+    window.addEventListener( 'resize', this.handleResize);
+
+    if (this.state.parallax) {
+      window.addEventListener( 'mousemove', this.handleMouseMove);
+    }
+  }
+
+
+  componentWillUnmount() {
+    window.removeEventListener( 'resize', this.handleResize);
+
+    if (this.state.parallax) {
+      window.removeEventListener( 'mousemove', this.handleMouseMove);
+    }
+  }
+
+
+  normMouseX( value ) {
+    let { panelWidth } = this.state;
+    let norm = _normal(panelWidth, 0, (panelWidth / 2), (-1 * (panelWidth / 2)));
+    return norm(value);
+  }
+
+
+  normMouseY( value ) {
+    let { panelHeight } = this.state;
+    let norm = _normal(panelHeight, 0, (panelHeight / 2), (-1 * (panelHeight / 2)));
+    return norm(value);
   }
 
 
   handleMouseMove( event ) {
     let { clientX, clientY } = event;
+    // maps mouse pos relative to the center of the panelcoordinates of the mouse, relative to
+    // the size of the panel
+    let mouseX = this.normMouseX(clientX);
+    let mouseY = this.normMouseY(clientY);
+
+    this.setState({ mouseX, mouseY });
+  }
+
+
+  handleResize() {
     let { offsetWidth, offsetHeight } = this.domNode;
     this.setState({
-      mouseX: int(((clientX - (offsetWidth / 2)) / (offsetWidth / 2)) * 100),
-      mouseY: int(((clientY - (offsetHeight / 2)) / (offsetHeight / 2)) * 100)
+      panelWidth: offsetWidth,
+      panelHeight: offsetHeight
     });
   }
 
 
-  // create a refrence to the DOM node to listen for mouseMove
+  // create a refrence to a DOM node to listen for mouseMove
   getElement( ref ) {
     this.domNode = ref;
+    this.setState({
+      panelWidth: ref.offsetWidth,
+      panelHeight: ref.offsetHeight
+    });
+  }
+
+
+  createBackground() {
+    let { project: { backgroundImg } } = this.props;
+    let { mouseX, mouseY, parallax, panelWidth, panelHeight } = this.state;
+
+    // the maxDistance is the distance from the center to the corner of the panel
+    let maxDistance = Math.sqrt(Math.pow(panelWidth, 2) + Math.pow(panelHeight, 2));
+
+    // the distance of the mouse, from the center of the panel, as a cartesian coordinate
+    let distance = Math.sqrt(Math.pow((mouseX), 2) + Math.pow((mouseY), 2));
+
+    let perspective = panelWidth < panelHeight
+      ? (panelHeight * 100 / panelWidth)
+      : (panelWidth  * 100 / panelHeight);
+
+    const backgroundStyle = (parallax)
+      ? {
+          ...styles.parallaxWrap,
+          WebkitPerspective: `${perspective}px`,
+          MozPerspective: `${perspective}px`,
+          OPerspective: `${perspective}px`,
+          perspective: `${perspective}px`,
+        }
+      : {
+          ...styles.background,
+          backgroundImage: `url(public/images/${backgroundImg})`
+        };
+
+
+    return (parallax)
+      ? (<div className="parallax-mouse" style={backgroundStyle}>
+          { backgroundImg.map( (fileName, i) => (
+            <div key={`bg-layer-${+i}-${fileName}`} style={styles.parallaxDiv}>
+              <img
+                src={`public/images/${fileName}`}
+                alt={fileName}
+                style={{
+                  width: `${panelWidth}px`,
+                  height: `${panelHeight}px`,
+                  [transform]: `
+                    translateZ(${(2 * distance / maxDistance) * Math.sqrt(perspective * i)}px)
+                    rotateY(${(mouseX * -1) / (panelWidth / 2)}deg)
+                    rotateX(${mouseY / (panelHeight / 2)}deg)
+                    `
+                }}
+              />
+            </div>
+          ))}
+        </div>)
+      : (<div style={backgroundStyle} />);
   }
 
 
   render() {
-    let {
-      style,
-      toggleModal,
-      project: { link, title, backgroundImg, shortDescription }
-    } = this.props;
-
-    let { mouseX, mouseY } = this.state;
-
-    let parallax = false;
-
-    let backgroundStyle, backgroundLayers;
-
-
-    if (Array.isArray(backgroundImg)) {
-      // create the parallax effect with layers
-      backgroundLayers = backgroundImg.map( (fileName, i) => (
-        <div
-          key={`bg-layer-${+i}-${fileName}`}
-          style={styles.parallaxDiv}>
-          <img
-            src={`public/images/${fileName}`}
-            alt={fileName}
-            style={{
-              [transform]: `translateZ(${int(backgroundImg.length + i)}px) rotateX(${mouseX * (i + 1)}deg) rotateY(${mouseY * (i + 1)}deg)`
-            }}
-          />
-        </div>
-      ));
-
-      backgroundLayers.unshift(
-        <div
-          key={`bg-layer-00-base`}
-          ref={this.getElement}
-          style={{...styles.background, backgroundColor: 'rgb(81, 81, 81)'}}
-        />
-      );
-
-      parallax = true;
-
-    } else {
-      backgroundStyle = (backgroundImg.length)
-        ? {...styles.background, backgroundImage: `url(public/images/${backgroundImg})`}
-        : styles.background;
-    }
+    let { style, toggleModal, project: { link, title, shortDescription } } = this.props;
 
     return (
-      <FillSection className="project-panel" style={{padding: 0}} onMouseMove={this.handleMouseMove}>
+      <FillSection className="project-panel" style={{padding: 0}}>
 
-        { parallax
-          ? (<div className="parallax-mouse" style={styles.parallaxWrap}>{backgroundLayers}</div>)
-          : (<div style={backgroundStyle} />)
-        }
+        <div className="background" ref={this.getElement} style={{width: '100%', height: '100%'}}>
+          { this.createBackground() }
+        </div>
 
         <Cell style={{position: null}}>
           <div style={styles.banner}>
@@ -195,107 +250,11 @@ class ProjectPanel extends Component {
             </div>
           </div>
         </Cell>
+
       </FillSection>
     );
   }
 }
-
-
-// const ProjectPanel = ({
-//   style,
-//   toggleModal,
-//   project: { link, title, backgroundImg, shortDescription }
-// }) => {
-//   let xDiff = null;
-//   let yDiff = null;
-//   let domNode = null;
-//
-//
-//   const getMousePos = (event) => {
-//     let { clientX, clientY } = event;
-//     let { offsetWidth, offsetHeight } = domNode;
-//     xDiff = int(((clientX - (offsetWidth / 2)) / (offsetWidth / 2)) * 100);
-//     yDiff = int(((clientY - (offsetHeight / 2)) / (offsetHeight / 2)) * 100);
-//   };
-//
-//
-//   // create the background elements based on the project.backgroundImg Prop
-//   const getBackground = (bgImg) => {
-//     // console.log(xDiff, yDiff, domNode.offsetWidth, `rotateX(${xDiff}deg)`);
-//
-//     if (Array.isArray(bgImg)) {
-//       // create the parallax effect with layers
-//       let backgroundLayers = bgImg.map( (fileName, i) => (
-//         <div
-//           key={`bg-layer-${+i}-${fileName}`}
-//           style={{...styles.background, backgroundColor: 'transparent'}}>
-//           <img
-//             src={`public/images/${fileName}`}
-//             alt={fileName}
-//             style={{
-//               [transform]: `translateZ(${int((bgImg.length / (i + 1)) * 10)}px)`
-//             }}
-//           />
-//         </div>
-//       ));
-//       backgroundLayers.unshift(
-//         <div
-//           key={`bg-layer-00-base`}
-//           ref={(ref) => { domNode = ref; }}
-//           style={{...styles.background, backgroundColor: 'rgb(81, 81, 81)'}}
-//         />
-//       );
-//
-//       return (
-//         <div className="parallax-mouse" style={styles.parallaxWrap}>
-//           { backgroundLayers }
-//         </div>
-//       );
-//     } else {
-//       let backgroundStyle = (bgImg.length)
-//         ? {...styles.background, backgroundImage: `url(public/images/${bgImg})`}
-//         : styles.background;
-//
-//       return (<div style={backgroundStyle} />);
-//     }
-//   };
-//
-//
-//   return (
-//     <FillSection className="project-panel" style={{padding: 0}} onMouseMove={getMousePos}>
-//
-//       { getBackground(backgroundImg) }
-//
-//       <Cell>
-//         <div style={styles.banner}>
-//           <div style={styles.bannerInfo}>
-//
-//             <Title style={style.title} parentStyle={styles.titleMain}>
-//               <span>{ title }</span>
-//             </Title>
-//
-//             <div>{ shortDescription }</div>
-//
-//             <div style={styles.buttonWrap}>
-//               <div style={{padding: '5px'}}>
-//                 <Button link={link} title={`Link To ${title}`}>
-//                   {'Check it out!'}
-//                 </Button>
-//               </div>
-//
-//               <div style={{padding: '5px'}}>
-//                 <Button title="More Info" onClick={toggleModal}>
-//                   {'More Info'}
-//                 </Button>
-//               </div>
-//             </div>
-//
-//           </div>
-//         </div>
-//       </Cell>
-//     </FillSection>
-//   );
-// };
 
 
 export default ProjectPanel;
