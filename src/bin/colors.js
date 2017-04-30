7ea77e57f7c55f8013ce3da1c590d899b9211712
Benjamin Saphier <b.saphier@gmail.com>
2017-04-30 16:40:34 -0400

    added color helper funcs for rgba

--- src/bin/colors.js
+++ src/bin/colors.js
@@ -9,3 +9,13 @@ export default {
   MENU_DARK: '#515151',             //'rgba(81, 81, 81, 1)'
   MENU_DARKER: '#2D2D2D',           //'rgba(45, 45, 45, 1)'
 };
+
+export const unmellowYellow = opacity => `rgba(255, 255, 102, ${opacity})`;
+
+export const coralRed = opacity => `rgba(255, 64, 64, ${opacity})`;
+export const amethyst = opacity => `rgba(144, 103, 198, ${opacity})`;
+export const operaMauve = opacity => `rgba(161, 136, 166, ${opacity})`;
+export const chineseViolet = opacity => `rgba(127, 90, 131, ${opacity})`;
+
+export const menuDark = opacity => `rgba(81, 81, 81, ${opacity})`;
+export const menuDarker = opacity => `rgba(45, 45, 45, ${opacity})`;
